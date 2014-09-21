3c79d663593eaeadeb7c4ab80d589840d8e5efe9
John Stryker <john.stryker@gmail.com>
2014-09-21 14:37:01 -0500

    Added base function to test commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
+## John Stryker - Programming Assignment 2 - 9/21/14
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
